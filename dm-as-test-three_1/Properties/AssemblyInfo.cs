bca8a8b5891c65c3ff0a35f4063d13086b669411
Lakshmi <bandarulakshminarasimham@gmail.com>
2023-06-23 18:35:14 +0530

    Add project files.

---
+++ dm-as-test-three_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dm-as-test-three_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("dm-as-test-three_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("BD8491E6-CECC-407A-9A37-0F51882217E2")]
+[assembly: AssemblyVersion("1.0.0.0")]
